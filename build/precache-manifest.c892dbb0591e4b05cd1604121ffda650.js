080e70427a77f86ab0679ce4ce59b12791e1e72e
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-08-31 17:01:10 +0300

    bla bla

---
+++ build/precache-manifest.c892dbb0591e4b05cd1604121ffda650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c0299c6305818f963c592b5efdb679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653c2a07909386f19161",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "18c0294e2bb52c4630be",
+    "url": "/static/js/2.f6fa85a0.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.f6fa85a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "653c2a07909386f19161",
+    "url": "/static/js/main.3954be50.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
