fadb17c8d9fa15847e1afefc76683b6a49ef83b2
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-09-01 17:38:37 +0300

    finish

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const Note = require('./models/note');
+
+const app = express();
 
 app.use(cors());
 app.use(express.static('./build'))
 app.use(express.json());
-
-const mongoose = require('mongoose')
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =
-  'mongodb+srv://TheAlmightyCrumb:@cluster0.cmrzo.mongodb.net/note-app?retryWrites=true&w=majority'
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
@@ -44,17 +31,15 @@ let notes = [
 ]
   
   app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    Note.find({}).then(notes => {
+      res.json(notes)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   const generateId = () => {
@@ -67,22 +52,19 @@ let notes = [
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -91,7 +73,7 @@ let notes = [
     response.status(204).end()
   })
   
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   })
